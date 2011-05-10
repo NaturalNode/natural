28495593aaf9e18d52caf577732c80b1b6a329df
Chris Umbel <chris@chrisumbel.com>
2011-05-10 18:56:40 -0400

    added the most basic of coverages for the bayes classifier

---
+++ spec/bayes_classifier_spec.js
@@ -0,0 +1,43 @@
+/*
+Copyright (c) 2011, Chris Umbel
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice,
+   this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+3. Neither the name of the PostgreSQL Global Development Group nor the names
+   of its contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+*/
+
+var classifier = require('lib/bayes_classifier');
+
+classifier.train([{classification: 'buy', text: "i am long qqqq"},
+              {classification: 'buy', text: "buy the q's"},
+              {classification: 'sell', text: "short gold"},
+              {classification: 'sell', text: "sell gold"}
+]);
+
+describe('bayes classifier', function() {
+    it('should classify', function() {
+        expect(classifier.classify('i am short silver')).toBe('sell');
+        expect(classifier.classify('i am long silver')).toBe('buy');        
+    });    
+});
