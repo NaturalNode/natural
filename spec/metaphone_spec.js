5a0c79906336e97df71cd157548f5e31a868158d
Chris Umbel <chris@chrisumbel.com>
2011-05-15 12:48:16 -0400

    beefed up step 2 tests

--- spec/metaphone_spec.js
+++ spec/metaphone_spec.js
@@ -12,6 +12,10 @@ describe('metaphone', function() {
     
     it('should drop some initial letters', function() {
         expect(metaphone.dropInitialLetters('knuth')).toBe('nuth');
+        expect(metaphone.dropInitialLetters('gnat')).toBe('nat');
+        expect(metaphone.dropInitialLetters('aegis')).toBe('egis');
+        expect(metaphone.dropInitialLetters('pneumatic')).toBe('neumatic');
+        expect(metaphone.dropInitialLetters('wrack')).toBe('rack');
     });
     
     it('should not drop other initial letters', function() {
