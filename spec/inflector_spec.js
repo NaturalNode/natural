68a47749592316f4a0f0c0c503fd930c3a865ee4
Chris Umbel <chris@chrisumbel.com>
2011-05-20 21:53:38 -0400

    added support for custom forms, but much feels duplicated now

--- spec/inflector_spec.js
+++ spec/inflector_spec.js
@@ -138,4 +138,13 @@ describe('inflector', function() {
         expect('mess'.pluralize()).toBe('messes');
         expect('messes'.singularize()).toBe('mess');
     });
+    
+    describe('should pluralize and singularize custom forms', function() {
+        var myInflector = new Inflector();
+        myInflector.attach();
+        myInflector.addPlural(/(code|ware)/, '$1z');
+        myInflector.addSingular(/(code|ware)z/, '$1');
+        expect('code'.pluralize()).toBe('codez');
+        expect('codez'.singularize()).toBe('code');        
+    });
 });
