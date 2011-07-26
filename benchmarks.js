88ed70ffffe0f193390682af7b7bccb84b397983
Tj Holowaychuk <tj@vision-media.ca>
2011-07-25 19:00:09 -0700

    Added small / medium benchmarks

--- benchmarks.js
+++ benchmarks.js
@@ -5,7 +5,8 @@
 
 var uubench = require('uubench')
   , natural = require('./')
-  , metaphone = natural.Metaphone.process;
+  , metaphone = natural.Metaphone.process
+  , fs = require('fs');
 
 var suite = new uubench.Suite({
   start: function(){
@@ -23,8 +24,30 @@ var suite = new uubench.Suite({
   }
 });
 
-suite.bench('metaphone()', function(next){
+// single word
+
+suite.bench('metaphone() word', function(next){
   metaphone('stephen');
+  next();
+});
+
+// small body of text
+
+var words = fs.readFileSync('lib/natural/index.js', 'utf8').split(/\W+/);
+suite.bench('metaphone() small', function(next){
+  for (var i = 0, len = words.length; i < len; ++i) {
+    metaphone(words[i]);
+  }
+  next();
+});
+
+// medium body of text
+
+var words2 = fs.readFileSync('README.md', 'utf8').split(/\W+/);
+suite.bench('metaphone() medium', function(next){
+  for (var i = 0, len = words2.length; i < len; ++i) {
+    metaphone(words2[i]);
+  }
   next();
 });
 
