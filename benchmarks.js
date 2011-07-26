6f5d1258f906e4ebe61571bc2d40189c7ac97fa5
Tj Holowaychuk <tj@vision-media.ca>
2011-07-25 18:54:48 -0700

    Added `make benchmark`

---
+++ benchmarks.js
@@ -0,0 +1,31 @@
+
+/**
+ * Module dependencies.
+ */
+
+var uubench = require('uubench')
+  , natural = require('./')
+  , metaphone = natural.Metaphone.process;
+
+var suite = new uubench.Suite({
+  start: function(){
+    console.log();
+  },
+
+  result: function(name, stats){
+    var persec = 1000 / stats.elapsed
+      , ops = stats.iterations * persec;
+    console.log('  \033[90m%s : \033[36m%s \033[90mops/s\033[0m', name, ops | 0);
+  },
+  
+  done: function(){
+    console.log();
+  }
+});
+
+suite.bench('metaphone()', function(next){
+  metaphone('stephen');
+  next();
+});
+
+suite.run();
