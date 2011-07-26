9780bf4c9e325ba0d6ad57f666d7e6a0f72976f1
Tj Holowaychuk <tj@vision-media.ca>
2011-07-25 19:20:00 -0700

    soundex benchmarks

--- benchmarks/metaphone.js
+++ benchmarks/metaphone.js
@@ -1,29 +1,4 @@
 
-/**
- * Module dependencies.
- */
-
-var uubench = require('uubench')
-  , natural = require('./')
-  , metaphone = natural.Metaphone.process
-  , fs = require('fs');
-
-var suite = new uubench.Suite({
-  start: function(){
-    console.log();
-  },
-
-  result: function(name, stats){
-    var persec = 1000 / stats.elapsed
-      , ops = stats.iterations * persec;
-    console.log('  \033[90m%s : \033[36m%s \033[90mops/s\033[0m', name, ops | 0);
-  },
-  
-  done: function(){
-    console.log();
-  }
-});
-
 // single word
 
 suite.bench('metaphone() word', function(next){
@@ -50,5 +25,3 @@ suite.bench('metaphone() medium', function(next){
   }
   next();
 });
-
-suite.run();
