dbe1e943c9ee8cb2ce1b4538b47c880c0ce16a52
Chris Umbel <chris@chrisumbel.com>
2011-08-14 15:55:22 -0400

    WordnetFile has its own open now

--- spec/wordnet_spec.js
+++ spec/wordnet_spec.js
@@ -26,104 +26,14 @@ var fileSearcher = require('lib/natural/wordnet/file_searcher'),
   Wordnet = require('lib/natural/wordnet/wordnet');
 
 describe('wordnet', function() {
-  describe('file_searcher', function() {
-    it('should find a line at the start of a file', function() {
-      fileSearcher.find('./spec/test_data/search_file', 'A', function(value) {
-        expect(value.key).toBe('A');
-        expect(value.line).toBe('A 1');
-        asyncSpecDone();
-      });
-      
-      asyncSpecWait();
-    });
-    
-    it('should find a line near the beginning in a file', function() {
-      fileSearcher.find('./spec/test_data/search_file', 'B', function(value) {
-        expect(value.key).toBe('B');
-        expect(value.line).toBe('B 2');
-        asyncSpecDone();
-      });
-      
-      asyncSpecWait();
-    });
-    
-    it('should find a line somewhere in a file 1', function() {
-      fileSearcher.find('./spec/test_data/search_file', 'D', function(value) {
-        expect(value.key).toBe('D');
-        expect(value.line).toBe('D 4');
-        asyncSpecDone();
-      });
-      
-      asyncSpecWait();
-    });
-    
-    it('should find a line near the end of a file', function() {
-      fileSearcher.find('./spec/test_data/search_file', 'K', function(value) {
-        expect(value.key).toBe('K');
-        expect(value.line).toBe('K 11');
-        asyncSpecDone();
-      });
-      
-      asyncSpecWait();
-    });
-    
-    it('should find a line near the end of a file', function() {
-      fileSearcher.find('./spec/test_data/search_file', 'I', function(value) {
-        expect(value.key).toBe('I');
-        expect(value.line).toBe('I 9');
-        asyncSpecDone();
-      });
-      
-      asyncSpecWait();
-    });
-    
-    it('should find a line at the end of a file', function() {
-      fileSearcher.find('./spec/test_data/search_file', 'M', function(value) {
-        expect(value.key).toBe('M');
-        expect(value.line).toBe('M 13');
-        asyncSpecDone();
-      });
-      
-      asyncSpecWait();
-    });
-    
-    it('should handle a miss in the middle', function() {
-      fileSearcher.find('./spec/test_data/search_file', 'B_NON_EXISTANT', function(value) {
-        expect(value).toEqual({status: 'miss'});
-        asyncSpecDone();
-      });
-      
-      asyncSpecWait();
-    });
-    
-    it('should handle a miss at the start', function() {
-      fileSearcher.find('./spec/test_data/search_file', '0_NON_EXISTANT', function(value) {
-        expect(value).toEqual({status: 'miss'});
-        asyncSpecDone();
-      });
-      
-      asyncSpecWait();
-    });
-    
-    it('should handle a miss at the end', function() {
-      fileSearcher.find('./spec/test_data/search_file', 'Z_NON_EXISTANT', function(value) {
-        expect(value).toEqual({status: 'miss'});
-        asyncSpecDone();
-      });
-      
-      asyncSpecWait();
-    });
-  });
-  
   describe('index_file', function() {
     it('should build a valid url', function() {
-      var indexFile = new IndexFile('./spec/test_data/wordnet', 'http://wordnet.naturalnode.com/', 'noun');
+      var indexFile = new IndexFile('./spec/test_data/wordnet', 'http://wordnet.naturalnode.com/', 'noun');      
       expect(indexFile.url).toBe('http://wordnet.naturalnode.com/index.noun');
     });
     
     it('should miss a record', function() {
-      var indexFile = new IndexFile('./spec/test_data/wordnet/', 'http://wordnet.naturalnode.com/', 'noun');
-      
+      var indexFile = new IndexFile('./spec/test_data/wordnet', 'http://wordnet.naturalnode.com/', 'noun');
       indexFile.lookup('aac', function(result) {
         expect(result).toBeNull();
         asyncSpecDone();
@@ -133,8 +43,7 @@ describe('wordnet', function() {
     });    
     
     it('should find a record', function() {
-      var indexFile = new IndexFile('./spec/test_data/wordnet/', 'http://wordnet.naturalnode.com/', 'noun');
-  
+      var indexFile = new IndexFile('./spec/test_data/wordnet', 'http://wordnet.naturalnode.com/', 'noun');      
       indexFile.lookup('pass', function(result) {
         expect(result.lemma).toBe('pass');
         expect(result.pos).toBe('n');
