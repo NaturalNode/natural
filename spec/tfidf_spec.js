51970dbce37c4f5a48f57a74573132e9fa308139
Chris Umbel <chris@chrisumbel.com>
2011-08-28 11:48:11 -0400

    fished up tfidf tests

--- spec/tfidf_spec.js
+++ spec/tfidf_spec.js
@@ -24,14 +24,64 @@ var Tfidf = require('lib/natural/tfidf/tfidf').TfIdf;
 
 var DEFAULT_IDF_UNITTEST = 1.5;
 var TEST_CORPUS = 'spec/test_data/tfdif_testcorpus.txt';
+var TEST_STOPWORDS = 'spec/test_data/tfdif_testcorpus.txt';
+
+var getExectedIdf = function(num_docs_total, num_docs_term){
+   return Math.log(parseFloat(1 + num_docs_total) / (1 + num_docs_term))
+}
 
 describe('tfidf', function() {
-    describe('non existant words', function() {
-        it('should return return the default value', function() {
+    describe('get idf', function() {
+        it('should return return the default value for non eixstant words', function() {
             var tfiObj = new Tfidf(TEST_CORPUS, null, DEFAULT_IDF_UNITTEST, function(myTfidf) {
                 expect(myTfidf.get_idf("THE")).toBe(DEFAULT_IDF_UNITTEST);
                 expect(myTfidf.get_idf("nonexistant")).toBe(DEFAULT_IDF_UNITTEST);                
             });
+        });
+        
+        it('should handle greaters', function() {
+            var tfiObj = new Tfidf(TEST_CORPUS, null, DEFAULT_IDF_UNITTEST, function(myTfidf) {
+                expect(myTfidf.get_idf("a")).toBeGreaterThan(myTfidf.get_idf("the"));
+            });
+        });
+        
+        it('should handle almost equal', function() {
+            var tfiObj = new Tfidf(TEST_CORPUS, null, DEFAULT_IDF_UNITTEST, function(myTfidf) {
+                expect(myTfidf.get_idf("girl")).toBe(myTfidf.get_idf("moon"));
+            });
+        });            
+    });
+    
+    describe('keywords', function() {
+        it('should retrieve keywords when there is only one keyword', function() {
+            var tfiObj = new Tfidf(TEST_CORPUS, TEST_STOPWORDS, DEFAULT_IDF_UNITTEST, function(myTfidf) {
+                expect(myTfidf.get_doc_keywords("the spoon and the fork")[0][0]).toBe("the");
+            });
+        });
+        
+        it('should retrieve multiple keywords', function() {
+            var tfiObj = new Tfidf(TEST_CORPUS, TEST_STOPWORDS, DEFAULT_IDF_UNITTEST, function(myTfidf) {
+                var keywords = myTfidf.get_doc_keywords("the girl said hello over the phone");
+                expect(keywords[0][0]).toBe("girl");
+                expect(keywords[1][0]).toBe("phone");
+                expect(keywords[2][0]).toBe("said");
+                expect(keywords[3][0]).toBe("the");                
+            });
+        });        
+    });
+    
+    describe('add corpus', function() {
+        var tfiObj = new Tfidf(TEST_CORPUS, null, DEFAULT_IDF_UNITTEST, function(myTfidf) {
+            expect(getExectedIdf(myTfidf.get_num_docs(), 1)).toBeTruthy();
+            expect(myTfidf.get_idf("water")).toBe(DEFAULT_IDF_UNITTEST);
+            expect(getExectedIdf(myTfidf.get_num_docs(), 1)).toBe(myTfidf.get_idf("moon"));
+            expect(getExectedIdf(myTfidf.get_num_docs(), 5)).toBe(myTfidf.get_idf("said"));
+            
+            myTfidf.add_input_document("water, moon");
+            
+            expect(getExectedIdf(myTfidf.get_num_docs(), 1)).toBe(myTfidf.get_idf("water"));
+            expect(getExectedIdf(myTfidf.get_num_docs(), 2)).toBe(myTfidf.get_idf("moon"));            
+            expect(getExectedIdf(myTfidf.get_num_docs(), 5)).toBe(myTfidf.get_idf("said"));            
         });
     });
 });
