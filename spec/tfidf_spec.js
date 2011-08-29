9c53fb54d63bbfef515530ac7f743e0443bd8877
Chris Umbel <chris@chrisumbel.com>
2011-08-28 23:21:08 -0400

    added ability for tfidf to measure strings

--- spec/tfidf_spec.js
+++ spec/tfidf_spec.js
@@ -25,7 +25,7 @@ var Tfidf = require('lib/natural/tfidf/tfidf').TfIdf;
 var DEFAULT_IDF_UNITTEST = 1.5;
 var TEST_CORPUS = 'spec/test_data/tfdif_testcorpus.txt';
 var TEST_STOPWORDS = 'spec/test_data/tfdif_testcorpus.txt';
-
+var TEST_CORPUS_STRING = '50\nthe:23\na:17\ngirl:1\nmoon:1\nsaid:5\nphone:2';
 var getExectedIdf = function(num_docs_total, num_docs_term){
    return Math.log(parseFloat(1 + num_docs_total) / (1 + num_docs_term))
 }
@@ -33,35 +33,62 @@ var getExectedIdf = function(num_docs_total, num_docs_term){
 describe('tfidf', function() {
     describe('get idf', function() {
         it('should return return the default value for non eixstant words', function() {
-            var tfiObj = new Tfidf(TEST_CORPUS, null, DEFAULT_IDF_UNITTEST, function(myTfidf) {
-                expect(myTfidf.getIdf("THE")).toBe(DEFAULT_IDF_UNITTEST);
-                expect(myTfidf.getIdf("nonexistant")).toBe(DEFAULT_IDF_UNITTEST);                
+            var tfObj = new Tfidf(DEFAULT_IDF_UNITTEST);
+            
+            tfObj.measureFile(TEST_CORPUS, null, function() {
+                expect(tfObj.getIdf("THE")).toBe(DEFAULT_IDF_UNITTEST);
+                expect(tfObj.getIdf("nonexistant")).toBe(DEFAULT_IDF_UNITTEST);                
+            });
+        });
+
+        it('should return return the default value for non eixstant words by string', function() {
+            var tfObj = new Tfidf(DEFAULT_IDF_UNITTEST);
+            
+            tfObj.measure(TEST_CORPUS_STRING, null, function() {
+                expect(tfObj.getIdf("THE")).toBe(DEFAULT_IDF_UNITTEST);
+                expect(tfObj.getIdf("nonexistant")).toBe(DEFAULT_IDF_UNITTEST);                
             });
         });
         
         it('should handle greaters', function() {
-            var tfiObj = new Tfidf(TEST_CORPUS, null, DEFAULT_IDF_UNITTEST, function(myTfidf) {
-                expect(myTfidf.getIdf("a")).toBeGreaterThan(myTfidf.getIdf("the"));
+            var tfObj = new Tfidf(DEFAULT_IDF_UNITTEST);
+            
+            tfObj.measureFile(TEST_CORPUS, null, function() {
+                expect(tfObj.getIdf("a")).toBeGreaterThan(tfObj.getIdf("the"));
+            });
+        });
+
+        it('should handle greaters by string', function() {
+            var tfObj = new Tfidf(DEFAULT_IDF_UNITTEST);
+            
+            tfObj.measure(TEST_CORPUS_STRING, null, function() {
+                expect(tfObj.getIdf("a")).toBeGreaterThan(tfObj.getIdf("the"));
             });
         });
         
         it('should handle almost equal', function() {
-            var tfiObj = new Tfidf(TEST_CORPUS, null, DEFAULT_IDF_UNITTEST, function(myTfidf) {
-                expect(myTfidf.getIdf("girl")).toBe(myTfidf.getIdf("moon"));
+            var tfObj = new Tfidf(DEFAULT_IDF_UNITTEST);
+            
+            tfObj.measureFile(TEST_CORPUS, null, function() {
+                expect(tfObj.getIdf("girl")).toBe(tfObj.getIdf("moon"));
             });
         });            
     });
     
     describe('keywords', function() {
         it('should retrieve keywords when there is only one keyword', function() {
-            var tfiObj = new Tfidf(TEST_CORPUS, TEST_STOPWORDS, DEFAULT_IDF_UNITTEST, function(myTfidf) {
-                expect(myTfidf.getDocKeywords("the spoon and the fork")[0][0]).toBe("the");
+            var tfObj = new Tfidf(DEFAULT_IDF_UNITTEST);
+            
+            tfObj.measureFile(TEST_CORPUS, TEST_STOPWORDS, function() {
+                expect(tfObj.getDocKeywords("the spoon and the fork")[0][0]).toBe("the");
             });
         });
         
         it('should retrieve multiple keywords', function() {
-            var tfiObj = new Tfidf(TEST_CORPUS, TEST_STOPWORDS, DEFAULT_IDF_UNITTEST, function(myTfidf) {
-                var keywords = myTfidf.getDocKeywords("the girl said hello over the phone");
+            var tfObj = new Tfidf(DEFAULT_IDF_UNITTEST);
+            
+            tfObj.measureFile(TEST_CORPUS, TEST_STOPWORDS, function() {
+                var keywords = tfObj.getDocKeywords("the girl said hello over the phone");
                 expect(keywords[0][0]).toBe("girl");
                 expect(keywords[1][0]).toBe("phone");
                 expect(keywords[2][0]).toBe("said");
@@ -71,17 +98,19 @@ describe('tfidf', function() {
     });
     
     describe('add corpus', function() {
-        var tfiObj = new Tfidf(TEST_CORPUS, null, DEFAULT_IDF_UNITTEST, function(myTfidf) {
-            expect(getExectedIdf(myTfidf.getNumDocs(), 1)).toBeTruthy();
-            expect(myTfidf.getIdf("water")).toBe(DEFAULT_IDF_UNITTEST);
-            expect(getExectedIdf(myTfidf.getNumDocs(), 1)).toBe(myTfidf.getIdf("moon"));
-            expect(getExectedIdf(myTfidf.getNumDocs(), 5)).toBe(myTfidf.getIdf("said"));
+        var tfObj = new Tfidf(DEFAULT_IDF_UNITTEST);
             
-            myTfidf.addInputDocument("water, moon");
+        tfObj.measureFile(TEST_CORPUS, null, function() {
+            expect(getExectedIdf(tfObj.getNumDocs(), 1)).toBeTruthy();
+            expect(tfObj.getIdf("water")).toBe(DEFAULT_IDF_UNITTEST);
+            expect(getExectedIdf(tfObj.getNumDocs(), 1)).toBe(tfObj.getIdf("moon"));
+            expect(getExectedIdf(tfObj.getNumDocs(), 5)).toBe(tfObj.getIdf("said"));
             
-            expect(getExectedIdf(myTfidf.getNumDocs(), 1)).toBe(myTfidf.getIdf("water"));
-            expect(getExectedIdf(myTfidf.getNumDocs(), 2)).toBe(myTfidf.getIdf("moon"));            
-            expect(getExectedIdf(myTfidf.getNumDocs(), 5)).toBe(myTfidf.getIdf("said"));            
+            tfObj.addInputDocument("water, moon");
+            
+            expect(getExectedIdf(tfObj.getNumDocs(), 1)).toBe(tfObj.getIdf("water"));
+            expect(getExectedIdf(tfObj.getNumDocs(), 2)).toBe(tfObj.getIdf("moon"));            
+            expect(getExectedIdf(tfObj.getNumDocs(), 5)).toBe(tfObj.getIdf("said"));            
         });
     });
 });
