3390c18c043f905674be3440d8ab7657ab4d76a0
Chris Umbel <chris@chrisumbel.com>
2012-02-11 14:31:26 -0500

    handled initial silent consonants

--- spec/double_metaphone_spec.js
+++ spec/double_metaphone_spec.js
@@ -1,7 +1,10 @@
 
-var metaphone = require('lib/natural/phonetics/double_metaphone');
+var doubleMetaphone = require('lib/natural/phonetics/double_metaphone');
 
 describe('double metaphone', function() {
-    it('should', function() {	
+    it('should drop initial silent consonants', function() {
+    	doubleMetaphone.process('gnat', function (spy) {
+			expect(spy.initialSilentConsonantSkipped).toBeTruthy();    		
+    	});
     });
 });
