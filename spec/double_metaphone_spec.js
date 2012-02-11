7e33017468df3bcfda4c1515599a91cc6ee828a1
Chris Umbel <chris@chrisumbel.com>
2012-02-11 15:07:13 -0500

    wrapped up test for initial vowel

--- spec/double_metaphone_spec.js
+++ spec/double_metaphone_spec.js
@@ -3,8 +3,18 @@ var doubleMetaphone = require('lib/natural/phonetics/double_metaphone');
 
 describe('double metaphone', function() {
     it('should drop initial silent consonants', function() {
-    	doubleMetaphone.process('gnat', function (spy) {
+    	var encodings = doubleMetaphone.process('gnat', function (spy) {
 			expect(spy.initialSilentConsonantSkipped).toBeTruthy();    		
     	});
+    });
+
+    it('should consider initial vowels to be A', function() {
+    	var encodings = doubleMetaphone.process('England');
+    	expect(encodings[0]).toMatch('^A.*');
+    	expect(encodings[1]).toMatch('^A.*');
+    	
+		encodings = doubleMetaphone.process('astromech');
+    	expect(encodings[0]).toMatch('^A.*');
+    	expect(encodings[1]).toMatch('^A.*');    	    	
     });
 });
