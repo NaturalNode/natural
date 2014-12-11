d5c03eedbd9b7e786775e9bd8d8c77e1de65b205
Malcolm <mbalchcr@oberlin.edu>
2014-12-10 20:16:38 -0500

    clarify function name, add tests for spelling corrector

--- spec/spellcheck_spec.js
+++ spec/spellcheck_spec.js
@@ -4,59 +4,55 @@ var Spellcheck = require('../lib/natural/spellcheck/spellcheck.js');
 
 describe('spellcheck', function() {
     
-    /*
-    describe('handled mistakes', function() {
-        var spellcheck = new Spellcheck();
+    describe('distance 1 corrections', function() {
+        var spellcheck = new Spellcheck(['something']);
         
         it('should fix deletions', function() {
-
+            expect(spellcheck.getCorrections('smething')).toContain('something');
         });
         it('should fix insertions', function() {
-
+            expect(spellcheck.getCorrections('somethhing')).toContain('something');
         });
         it('should fix transpositions', function() {
-
+            expect(spellcheck.getCorrections('somehting')).toContain('something');
         });
         it('should fix replacements', function() {
-
+            expect(spellcheck.getCorrections('sometzing')).toContain('something');
         });
     });
-    */
 
-/*
-    describe('special cases', function() {
-        var spellcheck = new Spellcheck('cat');
+    describe('distance 2 corrections', function() {
+        var spellcheck = new Spellcheck(['something']);
 
-        it('should not change a correct word', function() {
-
+        it('should fix a deletion then a transposition', function() {
+            expect(spellcheck.getCorrections('smtehing', 2)).toContain('something');
         });
     });
-    */
 
     describe('edits', function() {
         var spellcheck = new Spellcheck([]);
         var edits = spellcheck.edits('ab');
 
-        it('should compute insertions', function () {
+        it('should compute insertions', function() {
             var insertions = ['xab', 'axb', 'abx'];
             for(var i in insertions) {
                 expect(edits.indexOf(insertions[i])).toBeGreaterThan(-1);
             }
         });
-        it('should compute deletions', function () {
+        it('should compute deletions', function() {
             var deletions = ['a', 'b']; 
             for(var i in deletions) {
                 expect(edits.indexOf(deletions[i])).toBeGreaterThan(-1);
             }
         });
-        it('should compute transpositions', function () {
+        it('should compute transpositions', function() {
             var transpositions = ['ba']
             for(var i in transpositions) {
                 expect(edits.indexOf(transpositions[i])).toBeGreaterThan(-1);
             }
 
         });
-        it('should compute replacements', function () {
+        it('should compute replacements', function() {
             var replacements = ['zb', 'af']
             for(var i in replacements) {
                 expect(edits.indexOf(replacements[i])).toBeGreaterThan(-1);
@@ -67,9 +63,8 @@ describe('spellcheck', function() {
     describe('edits up to distance', function() {
         var spellcheck = new Spellcheck([]);
 
-        it('should compute some distance 2 edits', function () {
-            var edits = spellcheck.editsUpToDistance(['abc'], 2);
-            //TODO: More distance 2 edits
+        it('should compute some distance 2 edits', function() {
+            var edits = spellcheck.editsWithMaxDistance(['abc'], 2);
             var expectedEdits = [
                 'a', // two deletions
                 'abxzc', // two insertions
