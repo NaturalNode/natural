a89679a532d9b8e49e9d18e9a2375ffef9f4bd3e
xpanda17 <alif_bhaskoro@yahoo.com>
2017-10-05 17:43:25 +0700

    check plural form done

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var natural = require('./lib/natural/');
 var PorterStemmer = natural.PorterStemmerId;
-var lowercaseText = "ketika malaikat-malaikat  Cinta + cinta  + bertasbih";
-var tokens = PorterStemmer.tokenizeAndStem(lowercaseText);
-console.log(tokens);
+//var lowercaseText = "ketika malaikat-malaikat  Cinta + cinta  + bertasbih";
+//var tokens = PorterStemmer.tokenizeAndStem(lowercaseText);
+var text = "malaikat-malaikat"
+console.log(PorterStemmer.isPlural(text));
 //console.log(natural.PorterStemmer.tokenizeAndStem("fishing at lake")); // stem a single word
