d2338c569e807936e4ffe7a72d072a75b1e8636f
xpanda17 <alif_bhaskoro@yahoo.com>
2017-10-05 17:14:16 +0700

    tokenizer done

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var natural = require('./lib/natural/');
 var PorterStemmer = natural.PorterStemmerId;
-var lowercaseText = "ketika cinta bertasbih";
+var lowercaseText = "ketika malaikat-malaikat  Cinta + cinta  + bertasbih";
 var tokens = PorterStemmer.tokenizeAndStem(lowercaseText);
 console.log(tokens);
 //console.log(natural.PorterStemmer.tokenizeAndStem("fishing at lake")); // stem a single word
