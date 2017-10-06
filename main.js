cb55c9314934c50862831e4bf7aea8adfb7e77d9
xpanda17 <alif_bhaskoro@yahoo.com>
2017-10-07 01:17:41 +0700

    suffix tested already

--- main.js
+++ main.js
@@ -1,23 +1,42 @@
-// var natural = require('./lib/natural/');
-// var Stemmer = natural.StemmerId;
-// //var lowercaseText = "ketika malaikat-malaikat  Cinta + cinta  + bertasbih";
-// //var tokens = PorterStemmer.tokenizeAndStem(lowercaseText);
-// var text = "malaikat-malaikat"
-// //console.log(PorterStemmer.isPlural(text));
-// console.log(Stemmer.tokenizeAndStem("malaikat ku tak bersayap"));
-// console.log(Stemmer.a("berkilah"));
+var natural = require('./lib/natural/');
+var Stemmer = natural.StemmerId;
+data = []
 
-var Removal = require("./lib/natural/stemmers/indonesian/removal");
-var SuffixRules = require("./lib/natural/stemmers/indonesian/suffix_rules");
-sr = new SuffixRules();
-ts = sr.RemoveInflectionalParticle("biarkanlah");
-console.log(ts.current_word);
-console.log(ts.removal.getOriginalWord());
-console.log(ts.removal.getResult());
-console.log(ts.removal.getRemovedPart());
-console.log(ts.removal.getAffixType());
-// TESTCASES
-var testcases = ["mei","bui","nilai","hancurlah","benarkah","apatah","siapapun","jubahku","bajumu","celananya","hantui","belikan","jualan","bukumukah","miliknyalah","kulitkupun","berikanku","sakitimu","beriannya","kasihilah","dibuang","kesakitan","sesuap","beradu","berambut","bersuara","berdaerah","belajar","bekerja","beternak","terasing","teraup","tergerak","terpuruk","teterbang","melipat","meringkas","mewarnai","meyakinkan","membangun","memfitnah","memvonis","memperbarui","mempelajari","meminum","memukul","mencinta","mendua","menjauh","menziarah","menuklir","menangkap","menggila","menghajar","mengqasar","mengudara","mengupas","menyuarakan","mempopulerkan","pewarna","peyoga","peradilan","perumahan","permuka","perdaerah","pembangun","pemfitnah","pemvonis","peminum","pemukul","pencinta","pendahulu","penjarah","penziarah","penasihat","penangkap","penggila","penghajar","pengqasar","pengudara","pengupas","penyuara","pelajar","pelabuhan","petarung","terpercaya","pekerja","peserta","mempengaruhi","mengkritik","bersekolah","bertahan","mencapai","dimulai","petani","terabai","mensyaratkan","mensyukuri","mengebom","mempromosikan","memproteksi","memprediksi","pengkajian","pengebom","bersembunyi","bersembunyilah","pelanggan","pelaku","pelangganmukah","pelakunyalah","perbaikan","kebaikannya","bisikan","menerangi","berimanlah","memuaskan","berpelanggan","bermakanan","menyala","menyanyikan","menyatakannya","penyanyi","penyawaan","rerata","lelembut","lemigas","kinerja","buku-buku","berbalas-balasan","bolak-balik","bertebaran","terasingkan","membangunkan","mencintai","menduakan","menjauhi","menggilai","pembangunan","marwan","subarkah","memberdayakan","persemakmuran","keberuntunganmu","kesepersepuluhnya","siapakah memberdayakan pembangunan","Perekonomian","menahan","Cinta telah bertebaran.Keduanya saling mencintai.","(Cinta telah bertebaran)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  Keduanya saling mencintai.","peranan","memberikan","medannya","idealis","idealisme","finalisasi","penstabilan","pentranskripsi","mentaati","meniru-nirukan","menyepak-nyepak","melewati","menganga","kupukul","kauhajar","kuasa-Mu","malaikat-malaikat-Nya","nikmat-Ku","allah-lah"];
+data.push(['mei', 'mei'])
+data.push(['bui', 'bui'])
+
+data.push(['nilai', 'nilai'])
+
+data.push(['hancurlah', 'hancur'])
+data.push(['benarkah', 'benar'])
+data.push(['apatah', 'apa'])
+data.push(['siapapun', 'siapa'])
+
+data.push(['jubahku', 'jubah'])
+data.push(['bajumu', 'baju'])
+data.push(['celananya', 'celana'])
+data.push(['hantui', 'hantu'])
+data.push(['belikan', 'beli'])
+data.push(['jualan', 'jual'])
+data.push(['bukumukah', 'buku']) 
+data.push(['miliknyalah', 'milik'])
+data.push(['kulitkupun', 'kulit']) 
+data.push(['berikanku', 'beri'])
+data.push(['sakitimu', 'sakit'])
+data.push(['beriannya', 'beri'])
+data.push(['kasihilah', 'kasih'])
+
+str_output = "";
+for(var i in data){
+	sentence = data[i][0];
+	output   = Stemmer.stem(sentence);
+	str_output += output + " - ";
+}
+	
+console.log(str_output);
+
+// FULL TESTCASES
+// var testcases = ["mei","bui","nilai","hancurlah","benarkah","apatah","siapapun","jubahku","bajumu","celananya","hantui","belikan","jualan","bukumukah","miliknyalah","kulitkupun","berikanku","sakitimu","beriannya","kasihilah","dibuang","kesakitan","sesuap","beradu","berambut","bersuara","berdaerah","belajar","bekerja","beternak","terasing","teraup","tergerak","terpuruk","teterbang","melipat","meringkas","mewarnai","meyakinkan","membangun","memfitnah","memvonis","memperbarui","mempelajari","meminum","memukul","mencinta","mendua","menjauh","menziarah","menuklir","menangkap","menggila","menghajar","mengqasar","mengudara","mengupas","menyuarakan","mempopulerkan","pewarna","peyoga","peradilan","perumahan","permuka","perdaerah","pembangun","pemfitnah","pemvonis","peminum","pemukul","pencinta","pendahulu","penjarah","penziarah","penasihat","penangkap","penggila","penghajar","pengqasar","pengudara","pengupas","penyuara","pelajar","pelabuhan","petarung","terpercaya","pekerja","peserta","mempengaruhi","mengkritik","bersekolah","bertahan","mencapai","dimulai","petani","terabai","mensyaratkan","mensyukuri","mengebom","mempromosikan","memproteksi","memprediksi","pengkajian","pengebom","bersembunyi","bersembunyilah","pelanggan","pelaku","pelangganmukah","pelakunyalah","perbaikan","kebaikannya","bisikan","menerangi","berimanlah","memuaskan","berpelanggan","bermakanan","menyala","menyanyikan","menyatakannya","penyanyi","penyawaan","rerata","lelembut","lemigas","kinerja","buku-buku","berbalas-balasan","bolak-balik","bertebaran","terasingkan","membangunkan","mencintai","menduakan","menjauhi","menggilai","pembangunan","marwan","subarkah","memberdayakan","persemakmuran","keberuntunganmu","kesepersepuluhnya","siapakah memberdayakan pembangunan","Perekonomian","menahan","Cinta telah bertebaran.Keduanya saling mencintai.","(Cinta telah bertebaran)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  Keduanya saling mencintai.","peranan","memberikan","medannya","idealis","idealisme","finalisasi","penstabilan","pentranskripsi","mentaati","meniru-nirukan","menyepak-nyepak","melewati","menganga","kupukul","kauhajar","kuasa-Mu","malaikat-malaikat-Nya","nikmat-Ku","allah-lah"];
 /*for(var i in testcases){
 	console.log(testcases[i]);
 }*/
