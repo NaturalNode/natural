58fbb9e9b914f4b1e803db22e5c1ba73d5f6b580
xpanda17 <alif_bhaskoro@yahoo.com>
2017-10-06 16:56:07 +0700

    suffix rules done

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-var natural = require('./lib/natural/');
-var Stemmer = natural.StemmerId;
-//var lowercaseText = "ketika malaikat-malaikat  Cinta + cinta  + bertasbih";
-//var tokens = PorterStemmer.tokenizeAndStem(lowercaseText);
-var text = "malaikat-malaikat"
-//console.log(PorterStemmer.isPlural(text));
-console.log(Stemmer.tokenizeAndStem("malaikat ku tak bersayap"));
-console.log(Stemmer.a("berkilah"));
+// var natural = require('./lib/natural/');
+// var Stemmer = natural.StemmerId;
+// //var lowercaseText = "ketika malaikat-malaikat  Cinta + cinta  + bertasbih";
+// //var tokens = PorterStemmer.tokenizeAndStem(lowercaseText);
+// var text = "malaikat-malaikat"
+// //console.log(PorterStemmer.isPlural(text));
+// console.log(Stemmer.tokenizeAndStem("malaikat ku tak bersayap"));
+// console.log(Stemmer.a("berkilah"));
+
+var Removal = require("./lib/natural/stemmers/indonesian/removal");
+var SuffixRules = require("./lib/natural/stemmers/indonesian/suffix_rules");
+
+var x = 10;
+console.log("x : " + x);
+sr = new SuffixRules();
+rules = SuffixRules.rules;
+ths = rules[2](x);
+console.log("x : " + ths.x);
+console.log("y : " + ths.y);
+ths = rules[1](x);
+console.log("x : " + ths.x);
+console.log("y : " + ths.y);
