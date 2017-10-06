ff80a44df4648bc1f78c1723ea7e70b0b4eb058b
xpanda17 <alif_bhaskoro@yahoo.com>
2017-10-06 13:53:35 +0700

    35%

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 var natural = require('./lib/natural/');
-var PorterStemmer = natural.PorterStemmerId;
+var Stemmer = natural.StemmerId;
 //var lowercaseText = "ketika malaikat-malaikat  Cinta + cinta  + bertasbih";
 //var tokens = PorterStemmer.tokenizeAndStem(lowercaseText);
 var text = "malaikat-malaikat"
-console.log(PorterStemmer.isPlural(text));
-//console.log(natural.PorterStemmer.tokenizeAndStem("fishing at lake")); // stem a single word
+//console.log(PorterStemmer.isPlural(text));
+console.log(Stemmer.tokenizeAndStem("malaikat ku tak bersayap"));
+console.log(Stemmer.a("berkilah"));
