381fbf3b7d1cc6a7c9a302ee43ff6f6c0f193168
Hugo-ter-Doest <hwl.ter.doest@gmail.com>
2018-03-27 23:51:33 +0200

    Finished the afinn sentiment analysis

--- spec/SentimentAnalyzer_AFINN_spec.js
+++ spec/SentimentAnalyzer_AFINN_spec.js
@@ -1,17 +1,23 @@
 
+var stemmer = require('../lib/natural/stemmers/porter_stemmer');
 var Analyzer = require("../lib/natural/sentiment/Analyzer_AFINN");
-var analyzer = new Analyzer("English");
+var analyzer = new Analyzer("English", stemmer);
 var sentences = [
   "I like cherries",
-  "I don't like cherries",
+  "I like cherries very much",
+  "I do not like cherries",
+  "I hate cherries",
   "It seems that pollution is making things worse",
   "How are you doing?",
-  "I feel a lot better today"
+  "I am fine",
+  "I feel a lot better today",
+  "I feel great",
+  ""
 ];
 
 describe("The sentiment analyzer analyzes the sentiment in sententes", function() {
   it("should analyze a sentence correctly", function() {
-    sententes.forEach(function(s) {
+    sentences.forEach(function(s) {
       var words = s.split(/\s+/);
       console.log("Sentiment of \"" + s + "\": " + analyzer.getSentiment(words));
     });
