33e28a1d0aadd96bceafcd275d411f3786bb2e7f
Hugo W.L. ter Doest <hwl.ter.doest@gmail.com>
2021-01-26 08:11:41 +0100

    io_spec conforms to standard JS

--- benchmarks/stemmer_id.js
+++ benchmarks/stemmer_id.js
@@ -1,8 +1,13 @@
+const fs = require('fs')
+const natural = require('../')
+const stemmerId = natural.StemmerId
 
-var words = fs.readFileSync('benchmarks/sample-id.txt', 'utf8').split(/\W+/);
-suite.bench('stemmerId() sample text', function(next){
-  for (var i = 0, len = words.length; i < len; ++i) {
-    stemmerId.stem(words[i]);
+/* global suite */
+
+const words = fs.readFileSync('benchmarks/sample-id.txt', 'utf8').split(/\W+/)
+suite.bench('stemmerId() sample text', function (next) {
+  for (let i = 0, len = words.length; i < len; ++i) {
+    stemmerId.stem(words[i])
   }
-  next();
-});
+  next()
+})
