dc685d5521261277f31aeb35289adfaeec3ef96e
Hugo W.L. ter Doest <hwl.ter.doest@gmail.com>
2021-07-26 15:08:09 +0200

    Repaired Dice Coeffient  (issue #603)

--- spec/dice_coefficient_spec.js
+++ spec/dice_coefficient_spec.js
@@ -1,4 +1,5 @@
 'use strict'
+const fs = require('fs')
 
 const dice = require('../lib/natural/distance/dice_coefficient')
 
@@ -27,5 +28,15 @@ describe('dice', function () {
 
   it('should sanitize spacing', function () {
     expect(dice('the   space', 'the space')).toBe(1)
+  })
+
+  it('should compare complete texts', function () {
+    const text1 = require('./test_data/Wikipedia_EN_FrenchRevolution.json').text
+      //fs.readFileSync('spec/test_data/Wikipedia_EN_FrenchRevolution.txt',
+      //{ encoding: 'utf8', flag: 'r' })
+    const text2 = require('./test_data/Wikipedia_EN_InfluenceOfTheFrenchRevolution.json').text
+      //fs.readFileSync('spec/test_data/Wikipedia_EN_InfluenceOfTheFrenchRevolution.txt',
+      //{ encoding: 'utf8', flag: 'r' })
+    expect(dice(text1, text2)).toBe(0.7897503285151117)
   })
 })
