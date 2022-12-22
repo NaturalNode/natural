e69ff290e370d6c065222df3e4567db6a71fd536
Hugo W.L. ter Doest <hwl.ter.doest@gmail.com>
2022-12-22 15:46:28 +0100

    syntax

--- spec/tfidf_test.ts
+++ spec/tfidf_test.ts
@@ -26,7 +26,7 @@ tfidf.addDocument('this document is about ruby.')
 tfidf.addDocument('this document is about ruby and node.')
 
 tfidf.tfidfs('node ruby', function (i, measure) {
-  console.log('document #' + i.toString() + ' is ' + measure)
+  console.log('document #' + i.toString() + ' is ' + measure.toString())
 })
 tfidf.addDocument(['document', 'about', 'node'])
 tfidf.addDocument(['document', 'about', 'ruby'])
@@ -36,13 +36,13 @@ tfidf.addDocument(['document', 'about', 'node', 'node', 'examples'])
 tfidf.tfidfs(['node', 'ruby'], function (i, measure) {
   console.log('document #' + i.toString() + ' is ' + measure.toString())
 })
-tfidf.listTerms(0 /*document index*/).forEach(function (item) {
+tfidf.listTerms(0 /* document index */).forEach(function (item) {
   console.log(item.term + ': ' + item.tfidf.toString())
 })
 tfidf = new TfIdf()
 tfidf.addDocument('document one', 'un')
 tfidf.addDocument('document Two', 'deux')
-let s = JSON.stringify(tfidf)
+const s = JSON.stringify(tfidf)
 // save "s" to disk, database or otherwise
 
 // assuming you pulled "s" back out of storage.
