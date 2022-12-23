ab47648adf0c742939bb7a10e073c70d44259d2a
Hugo W.L. ter Doest <hwl.ter.doest@gmail.com>
2022-12-23 09:28:25 +0100

    syntax

--- spec/phonetics_test.ts
+++ spec/phonetics_test.ts
@@ -1,12 +1,12 @@
 import { Metaphone, SoundEx, DoubleMetaphone } from '../lib/natural/phonetics'
 
-var wordA = 'phonetics'
-var wordB = 'fonetix'
+const wordA = 'phonetics'
+const wordB = 'fonetix'
 if (Metaphone.compare(wordA, wordB)) console.log('they sound alike!')
 console.log(Metaphone.process('phonetics'))
 console.log(Metaphone.process('phonetics', 3))
 
-var encodings = DoubleMetaphone.process('Matrix')
+const encodings = DoubleMetaphone.process('Matrix')
 console.log(encodings[0])
 console.log(encodings[1])
 if (SoundEx.compare(wordA, wordB)) console.log('they sound alike!')
