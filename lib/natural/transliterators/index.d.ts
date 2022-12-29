8e1a13d9f1fc61230f92b62ada67f436756e3525
Hugo W.L. ter Doest <hwl.ter.doest@gmail.com>
2022-12-29 13:17:21 +0100

    add TS types to transliterators

--- lib/natural/transliterators/index.d.ts
+++ lib/natural/transliterators/index.d.ts
@@ -1,5 +1,6 @@
 /*
-Copyright (c) 2011, Chris Umbel
+Copyright (c) 2022,
+Hugo W.L. ter Doest <https://github.com/Hugo-ter-Doest>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -20,6 +21,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-'use strict'
-
-exports.TransliterateJa = require('./ja')
+// First define a function type for transliterators ...
+export declare type TransliteratorFunction = (s: string) => string
+// and then export the Japanese transliterator as a function of that type
+export declare let TransliterateJa: TransliteratorFunction
---
+++ spec/transliterators_test.ts
@@ -0,0 +1,7 @@
+
+import { TransliterateJa } from '../lib/natural/transliterators'
+
+console.log(TransliterateJa('あいうえお かきくけこ'))
+console.log(TransliterateJa('さしすせそ たちつてと'))
+console.log(TransliterateJa('アイウエオ カキクケコ'))
+console.log(TransliterateJa('サシスセソ タチツテト'))
