1451bd5cc9db8a5c4b3fd5a1b08adee33e396b97
Matt Mower <mdmower@cmphys.com>
2023-01-25 20:47:58 -0800

    Update sentiment TS declarations
    - Overload constructor to indicate only certain languages are available
      for a limited set of language types.

--- lib/natural/sentiment/index.d.ts
+++ lib/natural/sentiment/index.d.ts
@@ -23,7 +23,18 @@ THE SOFTWARE.
 
 import { Stemmer } from '../stemmers'
 
+export type AfinnLanguageType = 'afinn'
+export type AfinnLanguage = 'English' | 'Spanish' | 'Portuguese'
+
+export type SenticonLanguageType = 'senticon'
+export type SenticonLanguage = 'Spanish' | 'English' | 'Galician' | 'Catalan' | 'Basque'
+
+export type PatternLanguageType = 'pattern'
+export type PatternLanguage = 'Dutch' | 'Italian' | 'English' | 'French'
+
 export declare class SentimentAnalyzer {
-  constructor (language: string, stemmer: Stemmer, vocabulary: string)
+  constructor (language: AfinnLanguage, stemmer: Stemmer, type: AfinnLanguageType)
+  constructor (language: SenticonLanguage, stemmer: Stemmer, type: SenticonLanguageType)
+  constructor (language: PatternLanguage, stemmer: Stemmer, type: PatternLanguageType)
   getSentiment (words: string[]): number
 }
