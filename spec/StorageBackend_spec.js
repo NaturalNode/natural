6244684fc4c4d7af14e0aaf0bfde3d8ebde8a6b0
Hugo-ter-Doest <hwl.ter.doest@gmail.com>
2024-03-07 20:14:28 +0100

    Small improvements

--- spec/StorageBackend_spec.js
+++ spec/StorageBackend_spec.js
@@ -14,13 +14,13 @@ describe('StorageBackend', () => {
       attr1: 'val1',
       attr2: 'val2'
     }
-    // Javascript object in and out, filename is returned
+    // Javascript object in and out, filename is passed as key when retrieving
     it('should store data in and retrieve data from file', async () => {
       const options = { filename: 'example.txt' }
       await storageBackend.setStorageType(storage.STORAGE_TYPES.FILE)
       const result1 = await storageBackend.store(object, options)
       expect(result1).toEqual('example.txt')
-      const result2 = await storageBackend.retrieve(null, options)
+      const result2 = await storageBackend.retrieve(options.filename, {})
       expect(result2).toEqual(object)
     })
 
@@ -67,18 +67,21 @@ describe('StorageBackend', () => {
       expect(retrievedObject).toEqual(object)
     })
 
-    /*
     it('should throw an error for an invalid storage type', async () => {
-      const object = { key: 'value' }
-      const options = { filename: 'example.txt' }
-      storageBackend.setStorageType('invalidType')
       try {
-        await storageBackend.store(object, options)
-        fail('The store function should throw an error for an invalid storage type')
+        await storageBackend.setStorageType('invalidType')
       } catch (error) {
         expect(error.message).toEqual('Invalid storage type')
       }
     })
-    */
+
+    it('should throw an error for changing the storage type', async () => {
+      storageBackend.setStorageType('FILE')
+      try {
+        await storageBackend.setStorageType('FILE')
+      } catch (error) {
+        expect(error.message).toEqual('Storage type already set')
+      }
+    })
   }
 })
