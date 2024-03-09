06f987db09cdfb648b0666fbb02b3843d8258af0
Hugo-ter-Doest <hwl.ter.doest@gmail.com>
2024-03-09 17:16:36 +0100

    Simplified spec

--- spec/StorageBackend_spec.js
+++ spec/StorageBackend_spec.js
@@ -14,55 +14,16 @@ describe('StorageBackend', () => {
       attr1: 'val1',
       attr2: 'val2'
     }
-    // Javascript object in and out, filename is passed as key when retrieving
-    it('should store data in and retrieve data from file', async () => {
-      const options = { filename: 'example.txt' }
-      await storageBackend.setStorageType(storage.STORAGE_TYPES.FILE)
-      const key = await storageBackend.store(object, options)
-      // expect(result1).toEqual('example.txt')
-      const result2 = await storageBackend.retrieve(key, {})
-      expect(result2).toEqual(object)
-    })
 
-    // Javascript object in and out, key is set by the client in options and returned
-    it('should store data in and retrieve data from Memcached', async () => {
-      await storageBackend.setStorageType(storage.STORAGE_TYPES.MEMCACHED)
-      const returnKey = await storageBackend.store(object, {})
-      expect(returnKey).not.toEqual(null)
-      const result2 = await storageBackend.retrieve(returnKey, {})
-      expect(result2).toEqual(object)
-    })
-
-    // Javascript object in and out, MongoDB key object returned
-    it('should store data in and retrieve data from MongoDB', async () => {
-      await storageBackend.setStorageType(storage.STORAGE_TYPES.MONGODB)
-      await storageBackend.store(object, {}).then(id => {
-        expect(typeof id).toEqual('object')
-        storageBackend.retrieve(id, {}).then(retrievedObject => {
-          expect(retrievedObject).toEqual(object)
-        }).catch(error => {
-          console.log(error)
-        })
-      }).catch(error => {
-        console.log(error)
+    // A for loop to test all five storage methods
+    Object.keys(storage.STORAGE_TYPES).forEach(storageType => {
+      it('should store data in and retrieve data from ' + storageType, async () => {
+        await storageBackend.setStorageType(storageType)
+        const key = await storageBackend.store(object)
+        expect(key).not.toEqual(null)
+        const result = await storageBackend.retrieve(key)
+        expect(result).toEqual(object)
       })
-    })
-
-    // Javascript object in and out, key is set by the client in options and returned
-    it('should store data in and retrieve data from Redis', async () => {
-      await storageBackend.setStorageType(storage.STORAGE_TYPES.REDIS)
-      const returnedKey = await storageBackend.store(object, {})
-      expect(returnedKey).not.toEqual(null)
-      const retrievedObject = await storageBackend.retrieve(returnedKey, {})
-      expect(retrievedObject).toEqual(object)
-    })
-
-    // Javascript object in and out, key is set by Postgres and returned
-    it('should store data in and retrieve data from Postgres', async () => {
-      await storageBackend.setStorageType(storage.STORAGE_TYPES.POSTGRES, { tableName: 'lexicon6' })
-      const id = await storageBackend.store(object, {})
-      const retrievedObject = await storageBackend.retrieve(id)
-      expect(retrievedObject).toEqual(object)
     })
 
     it('should throw an error for an invalid storage type', async () => {
