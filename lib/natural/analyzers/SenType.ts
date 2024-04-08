a671bbf51e4d8f3b51370179118c0129a90cba3e
Hugo ter Doest <hwl.ter.doest@gmail.com>
2024-04-08 14:54:36 +0200

    Add ts_spec (#733)
    * Add ts_spec
    
    * Repair coverage reports
    
    * Removed test runs as these are run as part of coverage

--- lib/natural/analyzers/SenType.ts
+++ lib/natural/analyzers/SenType.ts
@@ -21,7 +21,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
-export enum SenType {
+export const enum SenType {
   Unknown = 'UNKNOWN',
   Command = 'COMMAND',
   Interrogative = 'INTERROGATIVE',
